5da38b8cec9b3acdd36269a2a18a5e2eb9a7c8a9
Sghen <945654877@qq.com>
2021-09-08 20:27:24 +0800

    [*]用户表手机号码类型

--- server/entity/init.sql
+++ server/entity/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE `user` (
 `id`  bigint NOT NULL ,
 `user_name`  varchar(255) NULL ,
 `password`  varchar(255) NULL ,
-`mobile`  int NOT NULL ,
+`mobile`  varchar(11) NOT NULL ,
 `level`  int NOT NULL DEFAULT 1,
 `dept_id`  bigint NULL ,
 `post_id`  bigint NULL ,
